c9d13e7a07b887bb2ff99ca22a25f7610c229ad4
TURPIN Jonathan <turpin.j@hotmail.fr>
2021-12-11 14:01:53 +0100

    [UPDATE] Nav CSS

--- front/src/Components/Layout/Nav/index.js
+++ front/src/Components/Layout/Nav/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 // imports
 import './nav.scss';
@@ -6,7 +7,39 @@ import './nav.scss';
 const Nav = () => {
     return (
         <header>
-            <nav>Nav</nav>
+            <NavLink
+                end
+                to="/j/history"
+                className="letterJ"
+                // activeClassName="letterJ-active"
+                exact
+            >
+                J
+            </NavLink>
+            <NavLink
+                to="/o/artWork"
+                className="letterO"
+                activeClassName="letterO-active"
+                exact
+            >
+                O
+            </NavLink>
+            <NavLink
+                to="/h/contact"
+                className="letterH"
+                activeClassName="letterH-active"
+                exact
+            >
+                H
+            </NavLink>
+            <NavLink
+                to="/n/skills"
+                className="letterN"
+                activeClassName="letterN-active"
+                exact
+            >
+                N
+            </NavLink>
         </header>
     );
 };
