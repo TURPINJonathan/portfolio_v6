0cb81b919e17c6b9c988ca479aa7dffad5582606
TURPIN Jonathan <turpin.j@hotmail.fr>
2021-12-29 23:53:44 +0100

    [ADD] Main page text && CSS

--- front/src/Components/Portfolio/index.js
+++ front/src/Components/Portfolio/index.js
@@ -6,7 +6,15 @@ import './portfolio.scss';
 const Portfolio = () => {
     return (
         <main>
-            <h1>Test</h1>
+            <section id="mainPage">
+                <p>Bienvenue sur le portfolio de</p>
+                <h1>Jonathan Turpin</h1>
+                <p>Développeur Web et Web Mobile</p>
+            </section>
+            <section id="mainCatch">
+                <p>Lorsqu'on est développeur web, tout est possible</p>
+                <p>tu n'as aucune limite, sinon celles que tu t'imposes</p>
+            </section>
         </main>
     );
 };
