b8a064cf759bf74ad0a18155d374a1ccd9f8a3b3
TURPIN Jonathan <turpin.j@hotmail.fr>
2021-12-30 02:05:16 +0100

    [ADD] Contact form structure done || TODO title && subtitle && media Q && requieres

--- front/src/Components/LetterH/index.js
+++ front/src/Components/LetterH/index.js
@@ -5,7 +5,60 @@ import './letterH.scss';
 
 const LetterH = () => {
     return (
-        <h1>LetterH</h1>
+        // TODO: Add a main and subtitle ("portfolio de Jonathan Turpin" "Formulaire de contact"
+        // TODO: Make the media queries
+        // TODO: Make the requiere
+        <main>
+            <form>
+                <fieldset>
+                    <legend>Coordonnées</legend>
+
+                    <div className="contactDetails">
+                        <div className="contactDetails__input">
+                            <label htmlFor="name">Nom</label>
+                            <input type="text" id="name" placeholder="Dupond" />
+                        </div>
+
+                        <div className="contactDetails__input">
+                            <label htmlFor="firstname">Prénom</label>
+                            <input type="text" id="firstname" placeholder="Jean" />
+                        </div>
+                    </div>
+
+                    <div className="contactDetails">
+                        <div className="contactDetails__input">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" placeholder="jean@dupond.com" />
+                        </div>
+                        <div className="contactDetails__input">
+                            <label htmlFor="phone">Téléphone</label>
+                            <input type="tel" id="phone" placeholder="06 01 02 03 04" />
+                        </div>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <legend>Votre message</legend>
+
+                    <div className="contactDetails__message">
+
+                        <div className="contactDetails__input">
+                            <label htmlFor="object">Objet</label>
+                            <input type="text" id="object" placeholder="Objet du message" />
+                        </div>
+
+                        <div className="contactDetails__input">
+                            <label htmlFor="message">Message</label>
+                            <textarea id="message" placeholder="Votre message" />
+                        </div>
+                    </div>
+                </fieldset>
+                <div className="buttons">
+                    <button className="true" type="submit">Envoyer</button>
+                    <button className="reset" type="reset">Réinitialiser</button>
+                    <button className="false" type="button">Annuler</button>
+                </div>
+            </form>
+        </main >
     );
 };
 
