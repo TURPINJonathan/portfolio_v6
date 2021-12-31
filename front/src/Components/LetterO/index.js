68e238c271111b821890410557718cbeed53d5a5
TURPIN Jonathan <turpin.j@hotmail.fr>
2021-12-31 12:44:41 +0100

    [ADD] ArtWork cards || TODO media queries

--- front/src/Components/LetterO/index.js
+++ front/src/Components/LetterO/index.js
@@ -5,7 +5,28 @@ import './letterO.scss';
 
 const LetterO = () => {
     return (
-        <h1>LetterO</h1>
+
+        // TODO: Make the media queries
+
+        <main>
+            <h2>Réalisations</h2>
+
+            <section id="cards">
+                <article className="card">
+                    <h3 className="card__title">nom du site</h3>
+                    <img className="card__picture" src="https://via.placeholder.com/300x200" alt="nom du site" />
+                    <p className="card__overview">description du site</p>
+                    <button className="card__details" >Détails</button>
+                </article>
+                <article className="card">
+                    <h3 className="card__title">nom du site</h3>
+                    <img className="card__picture" src="https://via.placeholder.com/300x200" alt="nom du site" />
+                    <p className="card__overview">description du site</p>
+                    <button className="card__details" >Détails</button>
+                </article>
+            </section>
+            <button className="reset">Page d'accueil</button>
+        </main>
     );
 };
 
