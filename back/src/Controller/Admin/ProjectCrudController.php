faf704f8b7e60db98ef66976f8c2c066668df777
TURPIN Jonathan <turpin.j@hotmail.fr>
2022-01-21 22:07:08 +0100

    [UPDATE] ProjectCrudController

--- back/src/Controller/Admin/ProjectCrudController.php
+++ back/src/Controller/Admin/ProjectCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Project;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ProjectCrudController extends AbstractCrudController
@@ -13,12 +16,12 @@ class ProjectCrudController extends AbstractCrudController
     }
 
 
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
+    // public function configureFields(string $pageName): iterable
+    // {
+    //     return [
+    //         IdField::new('id')->hideOnForm(),
+    //         TextField::new('name'),
+    //         TextEditorField::new('description'),
+    //     ];
+    // }
 }
