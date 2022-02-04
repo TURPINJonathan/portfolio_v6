b2ada3fedb1266d0ca8f1da2bb0de9aa9b7235bb
TURPIN Jonathan <turpin.j@hotmail.fr>
2022-02-04 14:48:28 +0100

    [UPDATE] Portfolio responsive DONE

--- front/src/Components/Portfolio/index.js
+++ front/src/Components/Portfolio/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Download } from "react-feather";
-import { Link } from "react-router-dom";
 
 import cv from "../../assets/cv_turpin_jonahtan.pdf";
 
@@ -13,20 +12,25 @@ const Portfolio = () => {
             <section id="mainPage">
                 <p>Bienvenue sur le portfolio de</p>
                 <h1>
-                    <span id="j1">J</span>
-                    <span id="o2">o</span>
-                    <span id="n3">n</span>
-                    <span id="a4">a</span>
-                    <span id="t5">t</span>
-                    <span id="h6">h</span>
-                    <span id="a7">a</span>
-                    <span id="n8">n </span>
-                    <span id="t10">T</span>
-                    <span id="u11">u</span>
-                    <span id="r12">r</span>
-                    <span id="p13">p</span>
-                    <span id="i14">i</span>
-                    <span id="n15">n</span>
+                    <div className="firstname">
+                        <span id="j1">J</span>
+                        <span id="o2">o</span>
+                        <span id="n3">n</span>
+                        <span id="a4">a</span>
+                        <span id="t5">t</span>
+                        <span id="h6">h</span>
+                        <span id="a7">a</span>
+                        <span id="n8">n </span>
+                    </div>
+                    <div className="lastname">
+                        <span id="t10">T</span>
+                        <span id="u11">u</span>
+                        <span id="r12">r</span>
+                        <span id="p13">p</span>
+                        <span id="i14">i</span>
+                        <span id="n15">n</span>
+                    </div>
+
                 </h1>
                 <p>Développeur Web et Web Mobile</p>
             </section>
