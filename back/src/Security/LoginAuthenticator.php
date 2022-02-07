67b88cdadf343b185fb61b5f9af7a00317695e5e
ExoCinki <aolacot@gmail.com>
2022-02-07 22:16:48 +0100

    Edit env and Redirect on LoginAuthenticator

--- back/src/Security/LoginAuthenticator.php
+++ back/src/Security/LoginAuthenticator.php
@@ -54,10 +54,8 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
+        return new RedirectResponse($this->urlGenerator->generate('admin'));
 
-        // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
