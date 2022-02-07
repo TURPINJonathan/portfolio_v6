5c8bf3c80f9bf3f9dbb0b645920b9ea5bcb3f127
ExoCinki <aolacot@gmail.com>
2022-02-07 21:56:24 +0100

    Edit security.yaml

--- back/src/Controller/SecurityController.php
+++ back/src/Controller/SecurityController.php
@@ -13,13 +13,14 @@ class SecurityController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        dd("ok");
+        
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
         // }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+        dd($error);
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
