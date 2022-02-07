24beeb83facf721bb89d8a8d59d7c8e3cd4171b9
ExoCinki <aolacot@gmail.com>
2022-02-07 21:56:47 +0100

    Edit security.yaml

--- back/src/Controller/SecurityController.php
+++ back/src/Controller/SecurityController.php
@@ -20,10 +20,10 @@ class SecurityController extends AbstractController
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        dd($error);
+        
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-
+        dd($lastUsername);   
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
