112ec3f070b2dba7410e45bf219728e1d8965571
ExoCinki <aolacot@gmail.com>
2022-02-07 20:22:39 +0100

    Change Redirect LoginAuth and Annotation on Login Logout

--- back/src/Controller/SecurityController.php
+++ back/src/Controller/SecurityController.php
@@ -25,9 +25,8 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    /**
-     * @Route("/logout", name="app_logout")
-     */
+
+    #[Route('/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
